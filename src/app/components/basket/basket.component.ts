2edbd1ccd5f1ab83ec5602fcc46599d09a34b233
Kamil Wiśniewski <kamilwisniewski92@gmail.com>
2017-06-19 10:38:37 +0200

    Update basket.component.ts

--- src/app/components/basket/basket.component.ts
+++ src/app/components/basket/basket.component.ts
@@ -3,64 +3,78 @@ import { HttpErrorHandler } from "../../core/services/http-error-handler";
 import { BasketService } from "./basket.service";
 import { Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized } from '@angular/router';
 import { ComponentComunicationService } from "./../../pages/buy/component-comunication.service";
-import {Basket} from '../../core/dto';
+import { Basket } from '../../core/dto';
+import { ChangeDetectorRef } from "@angular/core";
+import { ProductListComponent } from "./../product-list/product-list.component";
 
 @Component({
   selector: 'mpt-basket',
- // styleUrls: ['basket.component.css'],
+  // styleUrls: ['basket.component.css'],
   templateUrl: 'basket.component.html',
   providers: [],
 })
 export class BasketComponent implements OnInit {
   @Input() category: any;
-   basket: Basket[]=[];
-  checker:boolean=false;
-  price:number=0;
-  items:number=0;
+  basket: Basket[] = [];
+  checker: boolean = false;
+  price: number = 0;
+  items: number = 0;
 
-  constructor(private errorHandler: HttpErrorHandler, private basketService: BasketService) {
+  constructor(private errorHandler: HttpErrorHandler, private basketService: BasketService, private cd: ChangeDetectorRef) {
+  }
+  state: string = "Pokaż";
+  show1: boolean = false;
+  show() {
+    if (this.show1 == false) {
+      this.show1 = true;
+      this.state = "Ukryj";
+      this.ngOnInit();
+    } else {
+      this.show1 = false;
+      this.state = "Pokaż";
+    }
   }
 
 
   ngOnInit(): void {
     this.basketService.getProductByCategory()
-    .subscribe(u =>{
-      this.basket=u;  
-      this.checkerBasket();
+      .subscribe(u => {
+        this.basket = u;
+        this.checkerBasket();
       });
-    console.log("Koszyk posiada: "+this.basket)
-  
-    }
-
-    checkerBasket(){
-
-
- if(this.basket.length==0)
- {
-   this.checker=false;
- }else{
-   this.checker=true;
-     for (let entry of this.basket) {
-         this.items=this.items+entry.pieces;
-         this.price=this.price+(entry.product.price*entry.pieces);
-}
- }
-      
-
-    }
-
-
-  deleteBasket(){
-     if (!window.confirm('Jesteś pewien?')) return;
-    this.basketService.deleteAllBasket()
-    .subscribe(()=>{});
+    console.log("Koszyk posiada: " + this.basket)
   }
 
-  confirmTransaction(){
+  checkerBasket() {
+    if (this.basket.length == 0) {
+      this.checker = false;
+    } else {
+      this.checker = true;
+      for (let entry of this.basket) {
+        this.items = this.items + entry.pieces;
+        this.price = this.price + (entry.product.price * entry.pieces);
+      }
+    }
+
+
+  }
+
+
+  deleteBasket() {
+    if (!window.confirm('Jesteś pewien?')) return;
+    this.basketService.deleteAllBasket()
+      .subscribe(() => { });
+    this.cd.reattach();
+    this.ngOnInit();
+    this.ngOnInit();
+  }
+
+  confirmTransaction() {
     this.basketService.confirmTransaction()
-    .subscribe(()=>{});
-     this.basketService.deleteAllBasket()
-    .subscribe(()=>{});
+      .subscribe(() => { });
+    this.basketService.deleteAllBasket()
+      .subscribe(() => { });
+    this.cd.markForCheck();
 
   }
 
